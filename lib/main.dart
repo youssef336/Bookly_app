c846fd711333b389bef9939d4ba54c916ed0192f
youssef336 <youssefbelal2004@gmail.com>
2025-03-23 01:37:48 +0200

    create splash_view

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bookly_app/features/splash/presentation/views/splash_View.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_core/get_core.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const Bookly_app());
+}
+
+class Bookly_app extends StatelessWidget {
+  const Bookly_app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SplashView(),
+    );
+  }
+}
