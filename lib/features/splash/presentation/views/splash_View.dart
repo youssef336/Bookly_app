c7c8475d241fb32cd6e75507d6032310ab9fa63e
youssef336 <youssefbelal2004@gmail.com>
2025-03-23 01:45:19 +0200

    create SplashViewbody

--- lib/features/splash/presentation/views/splash_View.dart
+++ lib/features/splash/presentation/views/splash_View.dart
@@ -1,8 +1,9 @@
+import 'package:bookly_app/features/splash/presentation/views/widgets/splash_ViewBody.dart';
 import 'package:flutter/material.dart';
 
 class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Splash Screen')));
+    return const Scaffold(body: SplashViewbody());
   }
 }
