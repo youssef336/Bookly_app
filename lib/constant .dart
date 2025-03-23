693dc918aa94a8a3d6e4dae546b52c1b9a445f99
youssef336 <youssefbelal2004@gmail.com>
2025-03-23 22:21:27 +0200

    Refactor int state & navigation to home view

--- lib/constant .dart
+++ lib/constant .dart
@@ -1,3 +1,5 @@
 import 'package:flutter/material.dart';
 
 const KprimaryColor = Color(0XFF100B1F);
+
+const KTransationDuration = Duration(milliseconds: 250);
--- lib/features/splash/presentation/views/widgets/splash_ViewBody.dart
+++ lib/features/splash/presentation/views/widgets/splash_ViewBody.dart
@@ -1,6 +1,10 @@
+import 'package:bookly_app/constant%20.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/view/home.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/animation_splash_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+import 'package:get/utils.dart';
 
 class SplashViewbody extends StatefulWidget {
   const SplashViewbody({super.key});
@@ -16,15 +20,8 @@ class _SplashViewbodyState extends State<SplashViewbody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 1),
-    );
-    animation = Tween<Offset>(
-      begin: const Offset(0, 10),
-      end: const Offset(0, 0),
-    ).animate(animationController);
-    animationController.forward();
+    intanimation_splash_view();
+    navigatetohome();
   }
 
   @override
@@ -43,5 +40,27 @@ class _SplashViewbodyState extends State<SplashViewbody>
         animation_splash_view(animation: animation),
       ],
     );
+  }
+
+  void intanimation_splash_view() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+    animation = Tween<Offset>(
+      begin: const Offset(0, 10),
+      end: const Offset(0, 0),
+    ).animate(animationController);
+    animationController.forward();
+  }
+
+  void navigatetohome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(
+        () => const HomeView(),
+        transition: Transition.zoom,
+        duration: KTransationDuration,
+      );
+    });
   }
 }
