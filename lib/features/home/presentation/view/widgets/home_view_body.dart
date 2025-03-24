dfc664f1b5c81b87a0225d4dd486e6b88ac4027c
youssef336 <youssefbelal2004@gmail.com>
2025-03-24 16:28:59 +0200

    add CustomAppBarr to home

---
+++ lib/features/home/presentation/view/widgets/home_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: const [CustomAppBarr()]);
+  }
+}
