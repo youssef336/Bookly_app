ef78043c55f912ced69b1759e3e199ed7a7a5cc4
youssef336 <youssefbelal2004@gmail.com>
2025-03-26 22:09:38 +0200

    add Features List View Item to book detail

--- lib/features/home/presentation/view/widgets/books_details_view_body.dart
+++ lib/features/home/presentation/view/widgets/books_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly_app/features/home/presentation/view/widgets/custom_book_details_app_barr.dart';
+import 'package:bookly_app/features/home/presentation/view/widgets/features-list-view-item.dart';
 import 'package:flutter/material.dart';
 
 class BooksDetailsViewBody extends StatelessWidget {
@@ -6,9 +7,18 @@ class BooksDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      child: Column(children: const [CustomBookDetailsAppBarr()]),
+      child: Column(
+        children: [
+          const CustomBookDetailsAppBarr(),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * 0.24),
+            child: const FeaturesListViewItem(),
+          ),
+        ],
+      ),
     );
   }
 }
