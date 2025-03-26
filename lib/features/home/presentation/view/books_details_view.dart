7f29469fd3efea062e239a5ef82e0917940e8271
youssef336 <youssefbelal2004@gmail.com>
2025-03-26 20:30:19 +0200

    create books details view body

---
+++ lib/features/home/presentation/view/books_details_view.dart
@@ -0,0 +1,11 @@
+import 'package:bookly_app/features/home/presentation/view/widgets/books_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BooksDetailsView extends StatelessWidget {
+  const BooksDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: BooksDetailsViewBody());
+  }
+}
---
+++ lib/features/home/presentation/view/widgets/books_details_view_body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BooksDetailsViewBody extends StatelessWidget {
+  const BooksDetailsViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
