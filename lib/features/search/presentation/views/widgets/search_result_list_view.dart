ed8fcc7614402ef4717cb6c3d2d320b2f7ae7369
youssef336 <youssefbelal2004@gmail.com>
2025-03-29 17:59:11 +0200

    create book_model

---
+++ lib/features/search/presentation/views/widgets/search_result_list_view.dart
@@ -0,0 +1,22 @@
+import 'package:bookly_app/features/home/presentation/view/widgets/best_seller_list_view_item.dart';
+import 'package:flutter/material.dart';
+
+class SearchResultListView extends StatelessWidget {
+  const SearchResultListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: EdgeInsets.zero,
+      shrinkWrap: true,
+
+      itemCount: 10,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 16),
+          child: const BestSellerListViewItem(),
+        );
+      },
+    );
+  }
+}
--- lib/features/search/presentation/views/widgets/search_view_body.dart
+++ lib/features/search/presentation/views/widgets/search_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:bookly_app/features/home/presentation/view/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/features/search/presentation/views/widgets/custom_search_text_feild.dart';
+import 'package:bookly_app/features/search/presentation/views/widgets/search_result_list_view.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -21,26 +22,6 @@ class SearchViewBody extends StatelessWidget {
           Expanded(child: SearchResultListView()),
         ],
       ),
-    );
-  }
-}
-
-class SearchResultListView extends StatelessWidget {
-  const SearchResultListView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      padding: EdgeInsets.zero,
-      shrinkWrap: true,
-
-      itemCount: 10,
-      itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 16),
-          child: const BestSellerListViewItem(),
-        );
-      },
     );
   }
 }
