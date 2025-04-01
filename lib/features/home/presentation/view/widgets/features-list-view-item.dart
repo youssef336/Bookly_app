0ff2e7cfb093ebb37f04e8114e9479b4776f9192
youssef336 <youssefbelal2004@gmail.com>
2025-04-02 01:38:46 +0200

    create Cached Network Image

--- lib/features/home/presentation/view/widgets/features-list-view-item.dart
+++ lib/features/home/presentation/view/widgets/features-list-view-item.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: file_names
 
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class FeaturesListViewItem extends StatelessWidget {
@@ -9,15 +10,14 @@ class FeaturesListViewItem extends StatelessWidget {
   final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.4 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: NetworkImage(imageUrl),
-            fit: BoxFit.fill,
-          ),
-          borderRadius: BorderRadius.circular(16),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16),
+      child: AspectRatio(
+        aspectRatio: 2.4 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
         ),
       ),
     );
